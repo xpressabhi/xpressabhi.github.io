4325daa160753c6f33dc8843711a1a8379aecaaf
Abhishek Kumar Maurya <akmnitt@gmail.com>
2021-01-18 11:27:53 +0530

    Update lazyload.js

--- js/lazyload.js
+++ js/lazyload.js
@@ -1,24 +1,20 @@
-const images = document.querySelectorAll("[data-src]");
 
-function preloadImage(img) {
-  const src = img.getAttribute("data-src");
-  if (!src) {
-    return;
-  }
-  img.src = src;
+if ("IntersectionObserver" in window) {
+  const options = {
+    threshold: 1,
+    rootMargin: "0px 0px 200px 0px"
+  };
+  const images = document.querySelectorAll("[data-src]");
+  const imageObserver = new IntersectionObserver((entries, imageObserver) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        let lazyImage = entry.target;
+        lazyImage.src = lazyImage.dataset.src;
+        imageObserver.unobserve(lazyImage);
+      }
+    });
+  }, options);
+  images.forEach(img => imageObserver.observe(img));  
+} else{
+  console.log('IntersectionObserver is not supported.');
 }
-const options = {
-  threshold: 1,
-  rootMargin: "0px 0px 200px 0px"
-};
-const imageObserver = new IntersectionObserver((entries, imageObserver) => {
-  entries.forEach((entry) => {
-    if (!entry.isIntersecting) {
-      return;
-    } else {
-      preloadImage(entry.target);
-      imageObserver.unobserve(entry.target);
-    }
-  });
-}, options);
-images.forEach(img => imageObserver.observe(img));
